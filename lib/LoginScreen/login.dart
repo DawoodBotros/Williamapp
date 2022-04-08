47a3885fbf6a9daf16a05a3162abbe94cc06eeb9
Dawood Botros <dododawood236@gmail.com>
2022-04-08 17:43:48 +0200

    first commit

---
+++ lib/LoginScreen/login.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  var formkey = GlobalKey<FormState>();
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                colors: [Colors.blue, Colors.deepPurple, Colors.red])),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Center(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(right: 40, top: 40),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(fontSize: 35, color: Colors.white),
+                      ),
+                    ),
+                    Image(
+                      height: 100,
+                      image: AssetImage(
+                        'assets/images/OIP2.jpg',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              width: 400,
+              height: 400,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(25)),
+                gradient: LinearGradient(begin: Alignment.bottomLeft, colors: [
+                  Colors.blueGrey,
+                  Colors.white70,
+                  Colors.grey,
+                ]),
+              ),
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                    key: formkey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: emailController,
+                          validator: (value) {
+                            if (value!.isEmpty)
+                              {
+                                return 'يحتوي علي الايميل';
+                              }
+                            return null;
+                          },
+                          onChanged: (String value){
+                            print(value);
+                          },
+                          decoration: InputDecoration(
+                            enabledBorder: UnderlineInputBorder(),
+                            enabled: true,
+                            focusedBorder: UnderlineInputBorder(),
+                            hintText: "user name",
+                            labelText: "User Name",
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                        TextFormField(
+                          controller: passwordController,
+                          validator: (value) {
+                            if (value!.isEmpty)
+                            {
+                              return 'يحتوي علي رموز وحروف وارقام';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            enabledBorder: UnderlineInputBorder(),
+                            enabled: true,
+                            focusedBorder: UnderlineInputBorder(),
+                            hintText: "password",
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 25,
+                            ),
+                          ),
+                          keyboardType: TextInputType.visiblePassword,
+                          obscureText: true,
+                        ),
+                        RaisedButton(
+                          color: Colors.blue,
+                          onPressed: () {
+                           if (formkey.currentState!.validate())
+                             {
+                               print(emailController.text);
+                               print(passwordController.text);
+                             }
+                          },
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                            color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
